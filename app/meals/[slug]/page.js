129404647454c2322b543740574e8254082c30e7
paddlehands88 <stefancorlett@hotmail.com>
2024-08-14 08:25:30 +0100

    Lessons 128 to 134 - production build with revalidations, static&dynamic metadata. MODULE COMPLETED

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -4,6 +4,17 @@ import { notFound } from "next/navigation";
 import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
 
+export async function generateMetaData({ params }) {
+  const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 const MealDetailsPage = ({ params }) => {
   const meal = getMeal(params.slug);
 
